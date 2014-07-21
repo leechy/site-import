d2247cfa97abaa199e87f9274222f6475f4fdf09
Sergey Chikuyonok <serge.che@gmail.com>
2014-07-21 12:42:45 +0400

    Normalize file content in tests

--- test/suite.js
+++ test/suite.js
@@ -7,6 +7,14 @@ var chalk = require('chalk');
 var importer = require('../');
 var grunt = require('grunt');
 
+function normalize(text) {
+	return text.split(/\r?\n/)
+		.map(function(line) {
+			return line.trim()
+		})
+		.join('\n');
+}
+
 function compare(folder1, folder2) {
 	var list1 = glob.sync('**/*.*', {cwd: folder1}).sort();
 	var list2 = glob.sync('**/*.*', {cwd: folder2}).sort();
@@ -15,10 +23,10 @@ function compare(folder1, folder2) {
 
 	// compare file contents
 	list1.forEach(function(f) {
-		var content1 = fs.readFileSync(path.join(folder1, f));
-		var content2 = fs.readFileSync(path.join(folder2, f));
-		var hash1 = crc(content1);
-		var hash2 = crc(content2);
+		var content1 = fs.readFileSync(path.join(folder1, f), 'utf8');
+		var content2 = fs.readFileSync(path.join(folder2, f), 'utf8');
+		var hash1 = crc(normalize(content1));
+		var hash2 = crc(normalize(content2));
 
 		assert.equal(hash1, hash2, 'Comparing ' + f + ':\n\n' + content1 + '\n----------\n' + content2);
 	});
